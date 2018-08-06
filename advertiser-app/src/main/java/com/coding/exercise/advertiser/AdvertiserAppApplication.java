b4797645dbfb3dd4485fb4602630bd7fc4881aca
Kunal Sharma <kunalsha@live.com>
2018-08-05 18:17:44 -0700

    Initial Commit with Spring Boot starter code

---
+++ advertiser-app/src/main/java/com/coding/exercise/advertiser/AdvertiserAppApplication.java
@@ -0,0 +1,12 @@
+package com.coding.exercise.advertiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertiserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertiserAppApplication.class, args);
+	}
+}
---
+++ advertiser-app/src/test/java/com/coding/exercise/advertiser/AdvertiserAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coding.exercise.advertiser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvertiserAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
